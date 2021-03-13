b1fdeb6c818b306b43ad4039aa8a18604865b2e6
adamel2 <elddawork@gmail.com>
2021-03-13 22:26:32 +0200

    solve github page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/Navbar/Navbar";
 import FormInput from "./components/Form/FormInput";
 import ViewBlood from "./components/ViewBlood/ViewBlood";
 import Events from "./components/Event/Events";
-import { HashRouter as  Switch, Route, HashRouter, Redirect } from "react-router-dom";
+import { Switch, Route,Redirect, BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -14,17 +14,17 @@ function App() {
         <NavBar />
 
       </div>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       
       <Switch>
-          <Route exact path="/blood-bank" component={FormInput} />
-          <Route exact path="/blood-bank/ViewBlood" component={ViewBlood} />
-          <Route exact path="/blood-bank/events" component={Events} />
+          <Route exact path={process.env.PUBLIC_URL + '/blood-bank'} component={FormInput} />
+          <Route exact path={process.env.PUBLIC_URL + '/blood-bank/ViewBlood'} component={ViewBlood} />
+          <Route exact path={process.env.PUBLIC_URL + '/blood-bank/events'} component={Events} />
           <Redirect from="/" to="/blood-bank" />
         </Switch>
       
       
-     </HashRouter>
+     </BrowserRouter>
       <Footer />
     </div>
   );
